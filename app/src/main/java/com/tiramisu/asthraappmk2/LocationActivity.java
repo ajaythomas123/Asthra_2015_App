ab49c2f0da29d932a116b104e88919c7fca58ede
Rajeev Krishnan <rajeevkrishnan_r@yahoo.co.in>
2015-01-28 02:27:34 +0530

    Got The map module workin! :D

--- app/src/main/java/com/tiramisu/asthraappmk2/LocationActivity.java
+++ app/src/main/java/com/tiramisu/asthraappmk2/LocationActivity.java
@@ -3,16 +3,51 @@ package com.tiramisu.asthraappmk2;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+/*You need a separate dependency of google play services for this !*/
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class LocationActivity extends BaseActivity {
+
+public class LocationActivity extends BaseActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
         super.onBaseCreate("Location");
+        /*Just So you Know SJCET Location is:  9.726865, 76.726110 */
+
+        //The Intital Boring Map Setup, btw. the map id name is map.!
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+
     }
+
+    //Map Does its magical Works.. err.. operations here, like the markers and stuff! oh oh. also the camera pan animation. ( i like it personallly!)
+    @Override
+    public final void onMapReady(GoogleMap map){
+        map.setMyLocationEnabled(true);
+        map.addMarker(new MarkerOptions().position(new LatLng(9.726865, 76.726110 )).title("Asthra 2015 at SJCET").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(9.726865, 76.726110), 18),2000,null);
+
+        /*
+        * Da,
+        * Here we will manually need to add markers for events. okay.
+        * I had enuff of dynamic map s***t , so we will stick with the regualr static style.!
+        * */
+    }
+
 
 
     @Override
